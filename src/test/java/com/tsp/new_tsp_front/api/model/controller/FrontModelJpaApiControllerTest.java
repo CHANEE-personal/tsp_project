b47fe6ceae1e33c8153c2f36cd4077ee8b22e891
Cho Chan Hee <cksgml4424@gmail.com>
2022-02-10 21:59:04 +0900

    model junit 생성 커밋

---
+++ src/test/java/com/tsp/new_tsp_front/api/model/controller/FrontModelJpaApiControllerTest.java
@@ -0,0 +1,9 @@
+package com.tsp.new_tsp_front.api.model.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrontModelJpaApiControllerTest {
+
+}
