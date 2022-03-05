922bc7484e3205fef43863a37827d00730564f4a
Cho Chan Hee <cksgml4424@gmail.com>
2022-03-05 17:04:54 +0900

    model & production junit test case 추가

--- src/test/java/com/tsp/new_tsp_front/api/model/controller/FrontModelJpaApiControllerTest.java
+++ src/test/java/com/tsp/new_tsp_front/api/model/controller/FrontModelJpaApiControllerTest.java
@@ -40,11 +40,13 @@ class FrontModelJpaApiControllerTest {
     }
 
     @Test
-    @DisplayName("모델 상세 조회 테스트")
-    public void 모델상세조회() throws Exception {
+    @DisplayName("남성 모델 상세 조회 테스트")
+    public void 남성모델상세조회() throws Exception {
         mockMvc.perform(get("/api/model/1/3"))
                 .andDo(print())
                 .andExpect(status().isOk())
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.idx").value("3"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.categoryCd").value("1"))
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelFirstName").value("CHO"))
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelSecondName").value("CHAN HEE"))
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelKorFirstName").value("조"))
@@ -52,5 +54,41 @@ class FrontModelJpaApiControllerTest {
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.height").value("170"))
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.size3").value("34-24-34"))
                 .andExpect(jsonPath("$.modelInfoMap.modelInfo.shoes").value("270"));
+    }
+
+    @Test
+    @DisplayName("여성 모델 상세 조회 테스트")
+    public void 여성모델상세조회() throws Exception {
+        mockMvc.perform(get("/api/model/2/6"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.idx").value("6"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.categoryCd").value("2"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelFirstName").value("KIM"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelSecondName").value("YE YOUNG"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelKorFirstName").value("김"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelKorSecondName").value("예영"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.height").value("170"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.size3").value("31-21-31"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.shoes").value("220"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelMainYn").value("Y"));
+    }
+
+    @Test
+    @DisplayName("시니어 모델 상세 조회 테스트")
+    public void 시니어모델상세조회() throws Exception {
+        mockMvc.perform(get("/api/model/3/12"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.idx").value("12"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.categoryCd").value("3"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelFirstName").value("cho"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelSecondName").value("chanhee"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelKorFirstName").value("조"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelKorSecondName").value("찬희"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.height").value("170"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.size3").value("31-24-31"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.shoes").value("220"))
+                .andExpect(jsonPath("$.modelInfoMap.modelInfo.modelMainYn").value("Y"));
     }
 }
--- src/test/java/com/tsp/new_tsp_front/api/production/controller/FrontProductionJpaApiControllerTest.java
+++ src/test/java/com/tsp/new_tsp_front/api/production/controller/FrontProductionJpaApiControllerTest.java
@@ -3,41 +3,61 @@ package com.tsp.new_tsp_front.api.production.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tsp.new_tsp_front.api.portfolio.domain.FrontPortFolioDTO;
 import com.tsp.new_tsp_front.api.production.domain.FrontProductionDTO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.transaction.Transactional;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Transactional
-@AutoConfigureRestDocs(
-        outputDir = "target/snippets",
-        uriScheme = "http",
-        uriHost = "localhost",
-        uriPort = 80
-)
+@TestPropertySource(locations = "classpath:application-local.properties")
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class FrontProductionJpaApiControllerTest {
+
     @Autowired
-    protected MockMvc mvc;
+    private MockMvc mockMvc;
+
     @Autowired
-    protected ObjectMapper objectMapper;
+    private ObjectMapper objectMapper;
 
     @Test
+    @DisplayName("프로덕션 조회 테스트")
     public void 프로덕션조회() throws Exception {
-        FrontProductionDTO frontProductionDTO = new FrontProductionDTO();
+        mockMvc.perform(get("/api/production/lists").param("page", "1").param("size", "100"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productionList.length()", equalTo(55)));
+    }
 
+    @Test
+    @DisplayName("프로덕션 상세 조회 테스트")
+    public void 프로덕션상세조회() throws Exception {
+        // 사용
+        mockMvc.perform(get("/api/production/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productionInfo.productionInfo.idx").value("1"))
+                .andExpect(jsonPath("$.productionInfo.productionInfo.title").value("프로덕션 테스트"))
+                .andExpect(jsonPath("$.productionInfo.productionInfo.description").value("프로덕션 테스트"));
 
-        // given
-
-        // when
-
-        // then
+        // 미사용
+        mockMvc.perform(get("/api/production/39"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("NOT_FOUND_PRODUCTION"));
     }
 }
