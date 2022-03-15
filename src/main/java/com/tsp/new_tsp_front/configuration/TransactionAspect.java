d7a8f2058db82b7ea4274a6894028895bee078b4
Cho Chan Hee <cksgml4424@gmail.com>
2022-03-15 22:31:14 +0900

    transaction aop commit

---
+++ src/main/java/com/tsp/new_tsp_front/configuration/TransactionAspect.java
@@ -0,0 +1,47 @@
+package com.tsp.new_tsp_front.configuration;
+
+import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.interceptor.MatchAlwaysTransactionAttributeSource;
+import org.springframework.transaction.interceptor.RollbackRuleAttribute;
+import org.springframework.transaction.interceptor.RuleBasedTransactionAttribute;
+import org.springframework.transaction.interceptor.TransactionInterceptor;
+
+import java.util.Collections;
+
+@PropertySource("classpath:/application.properties")
+@EnableTransactionManagement
+@Configuration
+public class TransactionAspect {
+
+    private static final String AOP_TRANSACTION_METHOD_NAME="*";
+    private static final String AOP_TRANSACTION_EXPRESSION="execution(* com.tsp.new_tsp_front..*Repository.*(..))";
+
+    @Autowired
+    private PlatformTransactionManager transactionManager;
+
+    @Bean
+    public TransactionInterceptor transactionAdvice(){
+        MatchAlwaysTransactionAttributeSource source = new MatchAlwaysTransactionAttributeSource();
+        RuleBasedTransactionAttribute transactionAttribute = new RuleBasedTransactionAttribute();
+        transactionAttribute.setName(AOP_TRANSACTION_METHOD_NAME);
+        transactionAttribute.setRollbackRules(Collections.singletonList(new RollbackRuleAttribute(Exception.class)));
+        source.setTransactionAttribute(transactionAttribute);
+
+        return new TransactionInterceptor(transactionManager, source);
+    }
+
+    @Bean
+    public Advisor transactionAdviceAdvisor(){
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression(AOP_TRANSACTION_EXPRESSION);
+        return new DefaultPointcutAdvisor(pointcut, transactionAdvice());
+    }
+}
