d0995a64483608d6cd731d6210d5f28f4ea7c38b
CHO CHAN HEE <cksgml4424@gmail.com>
2022-08-24 11:21:46 +0900

    agency 좋아요 service 관련 커밋 #24

--- src/main/java/com/tsp/new_tsp_front/api/agency/service/FrontAgencyJpaService.java
+++ src/main/java/com/tsp/new_tsp_front/api/agency/service/FrontAgencyJpaService.java
@@ -5,6 +5,7 @@ import com.tsp.new_tsp_front.api.agency.domain.FrontAgencyEntity;
 import com.tsp.new_tsp_front.api.agency.service.impl.FrontAgencyJpaRepository;
 import com.tsp.new_tsp_front.exception.TspException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -69,6 +70,43 @@ public class FrontAgencyJpaService {
             return this.frontAgencyJpaRepository.findOneAgency(frontAgencyEntity);
         } catch (Exception e) {
             throw new TspException(NOT_FOUND_AGENCY, e);
+        }
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : favoriteAgencyCount
+     * 2. ClassName  : FrontAgencyJpaService.java
+     * 3. Comment    : 프론트 > Agency 좋아요 갯수 조회
+     * 4. 작성자       : CHO
+     * 5. 작성일       : 2022. 08. 24.
+     * </pre>
+     */
+    @Transactional(readOnly = true)
+    public Integer favoriteModelCount(Integer idx) throws TspException {
+        try {
+            return this.frontAgencyJpaRepository.favoriteAgencyCount(idx);
+        } catch (Exception e) {
+            throw new TspException(NOT_FOUND_AGENCY, e);
+        }
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : favoriteAgency
+     * 2. ClassName  : FrontAgencyJpaService.java
+     * 3. Comment    : 프론트 > Agency 좋아요
+     * 4. 작성자       : CHO
+     * 5. 작성일       : 2022. 08. 24.
+     * </pre>
+     */
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    public Integer favoriteAgency(FrontAgencyEntity frontAgencyEntity) throws TspException {
+        try {
+            return this.frontAgencyJpaRepository.favoriteAgency(frontAgencyEntity);
+        } catch (Exception e) {
+            throw new TspException(ERROR_AGENCY_LIKE, e);
         }
     }
 }
