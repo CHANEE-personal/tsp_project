ceb2d0c5985feffb5f297fa16c24338af8e6b0ad
CHO CHAN HEE <cksgml4424@gmail.com>
2022-09-01 09:58:21 +0900

    모델 스케줄 관련 controller, test case 추가 #28

---
+++ src/test/java/com/tsp/new_tsp_front/api/model/schedule/controller/FrontScheduleJpaApiControllerTest.java
@@ -0,0 +1,62 @@
+package com.tsp.new_tsp_front.api.model.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.event.EventListener;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@ExtendWith(RestDocumentationExtension.class)
+@TestPropertySource(locations = "classpath:application.properties")
+@TestConstructor(autowireMode = ALL)
+@RequiredArgsConstructor
+@AutoConfigureTestDatabase(replace = NONE)
+@DisplayName("모델 스케줄 Api Test")
+class FrontScheduleJpaApiControllerTest {
+    private MockMvc mockMvc;
+    private final WebApplicationContext wac;
+
+    @BeforeEach
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() {
+        this.mockMvc = webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
+                .alwaysExpect(status().isOk())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("모델 스케줄 조회 테스트")
+    void 모델스케줄조회테스트() throws Exception {
+        mockMvc.perform(get("/api/schedule/lists").param("page", "1").param("size", "100"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.scheduleList.length()", equalTo(55)));
+    }
+}
