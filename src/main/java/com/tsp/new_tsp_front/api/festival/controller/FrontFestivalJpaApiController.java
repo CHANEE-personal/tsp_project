e6a17df254229238671e0fd417f532077c02c4da
CHO CHAN HEE <cksgml4424@gmail.com>
2023-01-09 15:08:10 +0900

    모델 행사 관련 controller commit #72

---
+++ src/main/java/com/tsp/new_tsp_front/api/festival/controller/FrontFestivalJpaApiController.java
@@ -0,0 +1,100 @@
+package com.tsp.new_tsp_front.api.festival.controller;
+
+import com.tsp.new_tsp_front.api.festival.domain.FrontFestivalDTO;
+import com.tsp.new_tsp_front.api.festival.domain.FrontFestivalEntity;
+import com.tsp.new_tsp_front.api.festival.service.FrontFestivalJpaService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.rmi.ServerError;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+@Api(tags = "행사관련 API")
+@RequestMapping("/api/festival")
+public class FrontFestivalJpaApiController {
+
+    private final FrontFestivalJpaService frontFestivalJpaService;
+
+    /**
+     * <pre>
+     * 1. MethodName : findFestivalGroup
+     * 2. ClassName  : FrontFestivalJpaApiController.java
+     * 3. Comment    : 프론트 > 행사 리스트 조회
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2023. 01. 09.
+     * </pre>
+     */
+    @ApiOperation(value = "행사 리스트 조회", notes = "행사 리스트를 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "행사 리스트 조회 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 404, message = "존재 하지 않음", response = HttpClientErrorException.NotFound.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @GetMapping("/list/{month}")
+    public ResponseEntity<Map<String, Object>> findFestivalGroup(@PathVariable Integer month) {
+        Map<String, Object> festivalMap = new HashMap<>();
+        festivalMap.put("festivalGroup", frontFestivalJpaService.findFestivalGroup(month));
+        return ResponseEntity.ok().body(festivalMap);
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : findFestivalList
+     * 2. ClassName  : FrontFestivalJpaApiController.java
+     * 3. Comment    : 프론트 > 행사 리스트 조회
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2022. 08. 23.
+     * </pre>
+     */
+    @ApiOperation(value = "행사 조회", notes = "행사를 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "행사 조회 성공", response = Map.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 404, message = "존재 하지 않음", response = HttpClientErrorException.NotFound.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @GetMapping("/list/{month}/{day}")
+    public ResponseEntity<Map<String, Object>> findFestivalList(@PathVariable Integer month, @PathVariable Integer day) {
+        Map<String, Object> festivalMap = new HashMap<>();
+        festivalMap.put("festivalList", frontFestivalJpaService.findFestivalList(FrontFestivalEntity.builder()
+                .festivalMonth(month).festivalDay(day).build()));
+        return ResponseEntity.ok().body(festivalMap);
+    }
+
+    /**
+     * <pre>
+     * 1. MethodName : findOneFestival
+     * 2. ClassName  : FrontFestivalJpaApiController.java
+     * 3. Comment    : 행사 상세 조회
+     * 4. 작성자      : CHO
+     * 5. 작성일      : 2023. 01. 09.
+     * </pre>
+     */
+    @ApiOperation(value = "행사 상세 조회", notes = "행사를 상세 조회한다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "행사 상세 조회 성공", response = FrontFestivalDTO.class),
+            @ApiResponse(code = 400, message = "잘못된 요청", response = HttpClientErrorException.BadRequest.class),
+            @ApiResponse(code = 401, message = "허용되지 않는 관리자", response = HttpClientErrorException.Unauthorized.class),
+            @ApiResponse(code = 403, message = "접근거부", response = HttpClientErrorException.class),
+            @ApiResponse(code = 404, message = "존재 하지 않음", response = HttpClientErrorException.NotFound.class),
+            @ApiResponse(code = 500, message = "서버 에러", response = ServerError.class)
+    })
+    @GetMapping("/{idx}")
+    public ResponseEntity<FrontFestivalDTO> findOneFestival(@PathVariable Long idx) {
+        return ResponseEntity.ok(frontFestivalJpaService.findOneFestival(idx));
+    }
+}
---
+++ src/test/java/com/tsp/new_tsp_front/api/festival/controller/FrontFestivalJpaApiControllerTest.java
@@ -0,0 +1,146 @@
+package com.tsp.new_tsp_front.api.festival.controller;
+
+import com.tsp.new_tsp_front.api.festival.domain.FrontFestivalEntity;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.event.EventListener;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.greaterThan;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
+import static org.springframework.test.context.TestConstructor.AutowireMode.ALL;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-local.properties")
+@TestConstructor(autowireMode = ALL)
+@RequiredArgsConstructor
+@AutoConfigureTestDatabase(replace = NONE)
+@DisplayName("행사 Api Test")
+class FrontFestivalJpaApiControllerTest {
+    private MockMvc mockMvc;
+    private final WebApplicationContext wac;
+    private final EntityManager em;
+
+    @BeforeEach
+    @EventListener(ApplicationReadyEvent.class)
+    public void setup() {
+        this.mockMvc = webAppContextSetup(wac)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))  // 필터 추가
+                .alwaysExpect(status().isOk())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("축제 리스트 갯수 그룹 조회")
+    void 축제리스트갯수그룹조회() throws Exception {
+        // 등록
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        FrontFestivalEntity festivalEntity = FrontFestivalEntity.builder()
+                .festivalTitle("축제 제목")
+                .festivalDescription("축제 내용")
+                .festivalMonth(dateTime.getMonthValue())
+                .festivalDay(dateTime.getDayOfMonth())
+                .festivalTime(dateTime)
+                .build();
+
+        em.persist(festivalEntity);
+
+        FrontFestivalEntity festivalEntity1 = FrontFestivalEntity.builder()
+                .festivalTitle("축제 제목")
+                .festivalDescription("축제 내용")
+                .festivalMonth(dateTime.getMonthValue())
+                .festivalDay(dateTime.getDayOfMonth())
+                .festivalTime(dateTime)
+                .build();
+
+        em.persist(festivalEntity1);
+        em.flush();
+        em.clear();
+
+        mockMvc.perform(get("/api/festival/list/{month}", dateTime.getMonthValue()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.festivalGroup.length()", greaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("축제리스트조회")
+    void 축제리스트조회() throws Exception {
+        // 등록
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        FrontFestivalEntity festivalEntity = FrontFestivalEntity.builder()
+                .festivalTitle("축제 제목")
+                .festivalDescription("축제 내용")
+                .festivalMonth(dateTime.getMonthValue())
+                .festivalDay(dateTime.getDayOfMonth())
+                .festivalTime(dateTime)
+                .build();
+
+        em.persist(festivalEntity);
+
+        FrontFestivalEntity festivalEntity1 = FrontFestivalEntity.builder()
+                .festivalTitle("축제 제목")
+                .festivalDescription("축제 내용")
+                .festivalMonth(dateTime.getMonthValue())
+                .festivalDay(dateTime.getDayOfMonth())
+                .festivalTime(dateTime)
+                .build();
+
+        em.persist(festivalEntity1);
+        em.flush();
+        em.clear();
+
+        mockMvc.perform(get("/api/festival/list/{month}/{day}", dateTime.getMonthValue(), dateTime.getDayOfMonth()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.festivalList.length()", greaterThan(0)));
+    }
+
+    @Test
+    @DisplayName("축제 상세 조회 테스트")
+    void 축제상세조회테스트() throws Exception {
+        // 등록
+        LocalDateTime dateTime = LocalDateTime.now();
+
+        FrontFestivalEntity festivalEntity = FrontFestivalEntity.builder()
+                .festivalTitle("축제 제목")
+                .festivalDescription("축제 내용")
+                .festivalMonth(dateTime.getMonthValue())
+                .festivalDay(dateTime.getDayOfMonth())
+                .festivalTime(dateTime)
+                .build();
+
+        em.persist(festivalEntity);
+
+        mockMvc.perform(get("/api/festival/{idx}", festivalEntity.getIdx()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=utf-8"))
+                .andExpect(jsonPath("$.idx").value(festivalEntity.getIdx()));
+    }
+}
