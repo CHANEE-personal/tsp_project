7a1fe855c93e05f1e8bcadb91760c5be5f61e38f
chochanhee <cksgml159@waug.com>
2023-03-05 14:20:33 +0900

    admin configurationProperties 적용 #97

--- admin-api/src/main/java/com/tsp/configuration/JasyptConfig.java
+++ admin-api/src/main/java/com/tsp/configuration/JasyptConfig.java
@@ -1,24 +1,26 @@
 package com.tsp.configuration;
 
+import com.tsp.configuration.info.JasyptInfo;
+import lombok.RequiredArgsConstructor;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class JasyptConfig {
 
-    @Value("${jasypt.encryptor.password}")
-    private String password;
+    private final JasyptInfo jasyptInfo;
+
 
     @Bean(name = "jasyptStringEncryptor")
     public StringEncryptor stringEncryptor() {
         PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
 
         SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-        config.setPassword(password);
+        config.setPassword(jasyptInfo.getPassword());
         config.setAlgorithm("PBEWithMD5AndDES");
         config.setKeyObtentionIterations("1000");
         config.setPoolSize("1");
